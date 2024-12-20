ba95a9307abd2d101ca8c28c644ed2a98b9f2e78
abdellahbouzekri <abdo.bouzekri123@gmail.com>
2024-12-20 19:18:43 +0100

    adding Cv

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -31,7 +31,7 @@ const Hero = () => {
             initial="hidden"
             animate="visible"
             variants={containerVariants}
-            className="flex flex-col items-center lg:items-start mt-10">
+            className="flex flex-col items-center mt-10 lg:items-start">
             <motion.h2
               variants={childVariants}
               className="pb-2 text-4xl tracking-tighter lg:text-7xl">
@@ -39,12 +39,12 @@ const Hero = () => {
             </motion.h2>
             <motion.span
               variants={childVariants}
-              className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent">
+              className="text-3xl tracking-tight text-transparent bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text">
               Full Stack Developer
             </motion.span>
             <motion.p
               variants={childVariants}
-              className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter ">
+              className="max-w-lg py-6 my-2 text-xl leading-relaxed tracking-tighter ">
               I'm a full-stack developer with a passion for building scalable
               and efficient web applications. With a strong foundation in
               computer science and a keen eye for design, I strive to create
@@ -52,10 +52,10 @@ const Hero = () => {
             </motion.p>
             <motion.a
               variants={childVariants}
-              href="/resume.pdf"
+              href="/Resume copy.pdf"
               target="_blank"
               rel="noopener noreferrer"
-              className="bg-white rounded-full p-4 text-sm text-stone-800 mb-16 hover:bg-indigo-600 hover:text-yellow-50"
+              className="p-4 mb-16 text-sm bg-white rounded-full text-stone-800 hover:bg-indigo-600 hover:text-yellow-50"
               download>
               Download Resume
             </motion.a>
